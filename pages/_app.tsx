94e1e111dd66fe28fdcea4d1228f604e39fe875d
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-06-01 09:03:16 -0300

    feat: initial config stitches css-injs lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
+import { globalStyle } from '../stitches.config'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyle()
   return <Component {...pageProps} />
 }
 
---
+++ stitches.config.ts
@@ -0,0 +1,17 @@
+import { createStitches } from '@stitches/react'
+
+const { styled, globalCss: GlobalCss, getCssText, theme, css, keyframes, createTheme } = createStitches({
+
+})
+
+const globalStyle =  GlobalCss({})
+
+export {
+  styled, 
+  getCssText, 
+  theme, 
+  css, 
+  keyframes, 
+  createTheme,
+  globalStyle
+}
